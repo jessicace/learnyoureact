73d03eb0e65c7d487f101b10e967361c5e68568a
Jessica Claire Edwards <jessica@eml.cc>
2015-10-18 15:32:12 +1100

    Solved first exercise

---
+++ program.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function(request, response) {
+    response.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
